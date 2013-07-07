6e5aedbfb93eac7a95b2ba0cb2c013c960b7b54e
robertom <r.malagigi@yahoo.co.uk>
2013-07-08 00:38:53 +0100

    new file and pom modified to cause merging problem

---
+++ my-first-scala-project/src/main/scala/org/binqua/scala/examples/general/HelloWorldInTestingBranch.scala
@@ -0,0 +1,10 @@
+package org.binqua.scala.examples.general;
+
+object HelloWorld{
+    def main(args: Array[String]){
+        println("hello")
+        // robertomalagigi comment
+        // this is for the testing branch
+        println("hello again")
+    }
+}
