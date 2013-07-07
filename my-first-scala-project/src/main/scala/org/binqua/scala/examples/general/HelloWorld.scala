739dcacaf9b16ea2d91ddeda53403cb3386b0b29
robertom <r.malagigi@yahoo.co.uk>
2013-07-07 23:00:59 +0100

    another commit on testing branch

--- my-first-scala-project/src/main/scala/org/binqua/scala/examples/general/HelloWorld.scala
+++ my-first-scala-project/src/main/scala/org/binqua/scala/examples/general/HelloWorld.scala
@@ -3,7 +3,8 @@ package org.binqua.scala.examples.general;
 object HelloWorld{
     def main(args: Array[String]){
         println("hello")
-       // robertomalagigi comment
-      // this is for the testing branch
+        // robertomalagigi comment
+        // this is for the testing branch
+        println("hello again")
     }
 }
