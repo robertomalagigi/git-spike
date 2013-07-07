6b6dad96e9f0e6c078e4dd89431607f184597403
robertom <r.malagigi@yahoo.co.uk>
2013-07-07 15:18:37 +0100

    added one big  comment

--- my-first-scala-project/src/main/scala/org/binqua/scala/examples/general/HelloWorld.scala
+++ my-first-scala-project/src/main/scala/org/binqua/scala/examples/general/HelloWorld.scala
@@ -2,6 +2,7 @@ package org.binqua.scala.examples.general;
 
 object HelloWorld{
     def main(args: Array[String]){
-        println("hello")
+        println("hello") //comment another comment
+      // robertomalagigi comment
     }
 }
